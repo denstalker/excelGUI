cd038e6c0ad30ae1bf4fdb160c34ff5dda8e1b86
Denis <brenkov_d@inbox.ru>
2019-10-20 21:04:05 +0300

    second commit

--- widget.cpp
+++ widget.cpp
@@ -16,7 +16,7 @@ Widget::Widget(QWidget *parent)
 
     connect(btnClose,SIGNAL(clicked()),this,SLOT(close()));
     //    // [1]  Writing excel file(*.xlsx)
-    //    QXlsx::Document xlsxW;
+    //    QXlsx::Document xlsixW;
     //    xlsxW.write("A1", "Hello Qt!"); // write "Hello Qt!" to cell(A,1). it's shared string.
     //    if ( xlsxW.saveAs("Test.xlsx") ) // save the document as 'Test.xlsx'
     //    {
@@ -29,29 +29,51 @@ Widget::Widget(QWidget *parent)
 
     //    qDebug() << "[debug] current directory is " << QDir::currentPath();
 
-        // [2] Reading excel file(*.xlsx)
-        Document xlsxR("1.xlsx");
+    // [2] Reading excel file(*.xlsx)
+    Document xlsxR("1.xlsx");
 
-        if ( xlsxR.load() ) // load excel file
+    if ( xlsxR.load() ) // load excel file
+    {
+        qDebug() << "[debug] success to load xlsx file.";
+
+        //            for (int i = 0; i < 50; i++)
+        //            {
+        //                qDebug() << i;
+        //                Cell* cell1 = xlsxR.cellAt(i,5) ;
+        //                QVariant var = cell1->readValue();
+        //                qDebug() << "Значение : " << var.toInt();
+        //            }
+
+
+        //            int row = 7; int col =5;
+        //           Cell* cell = xlsxR.cellAt(row, col); // get cell pointer.
+
+
+        Cell* cell;
+        for (int i = 7; i < 50; i++)
         {
-            qDebug() << "[debug] success to load xlsx file.";
-
-            int row = 7; int col =2;
-            Cell* cell = xlsxR.cellAt(row, col); // get cell pointer.
+            Cell* cell = xlsxR.cellAt(i, 5); // get cell pointer.
             if ( cell != NULL )
             {
-                QVariant var = cell->readValue(); // read cell value (number(double), QDateTime, QString ...)
-                qDebug() << "[debug] cell(1,1) is " << var; // Display value. It is 'Hello Qt!'.
+
+                QVariant var = cell->readValue();
+                qDebug() << "[" << i << "]" <<"Значение : " << var.toInt();
+
+
+                //                QVariant var = cell->readValue(); // read cell value (number(double), QDateTime, QString ...)
+                //                qDebug() << "[debug] cell(1,1) is " << var; // Display value. It is 'Hello Qt!'.
             }
             else
             {
                 qDebug() << "[debug][error] cell(1,1) is not set.";
             }
         }
-        else
-        {
-            qDebug() << "[debug][error] failed to load xlsx file.";
-        }
+
+    }
+    else
+    {
+        qDebug() << "[debug][error] failed to load xlsx file.";
+    }
 }
 
 Widget::~Widget()
